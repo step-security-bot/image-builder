265f9acd06571b88ab5d2554617f6877d7d3c413
Matt Boersma <Matt.Boersma@microsoft.com>
2023-10-28 11:27:18 -0600

    foo

--- images/capi/packer/azure/scripts/azure-compute-gallery.sh
+++ images/capi/packer/azure/scripts/azure-compute-gallery.sh
@@ -13,7 +13,7 @@ ${AZURE_CLIENT_SECRET}
 "
 eval "${tracestate}"
 
-# Flatcar environment specific variables.
+# Environment-specific variables.
 AZURE_SUBSCRIPTION_ID=${AZURE_SUBSCRIPTION_ID:-00000000-00000000-00000000-00000000}
 AZURE_TENANT_ID=${AZURE_TENANT_ID:-00000000-00000000-00000000-00000000}
 IMAGE_PUBLISHER_NAME=${IMAGE_PUBLISHER_NAME:-yourname}
@@ -22,35 +22,38 @@ IMAGE_PUBLISHER_CONTACT=${IMAGE_PUBLISHER_CONTACT:-infra@yourdomain}
 IMAGE_EULA_URL=${IMAGE_EULA_URL:-https://yoururl}
 VHD_STORAGE_ACCOUNT_NAME=${VHD_STORAGE_ACCOUNT_NAME:-yourazurestorageaccountname}
 
-# Generic Flatcar variables.
+# Generic variables.
 AZURE_LOCATION=${AZURE_LOCATION:-westeurope}
-PUBLISHING_SIG_RESOURCE_GROUP=${PUBLISHING_SIG_RESOURCE_GROUP:-flatcar-image-gallery-publishing}
-STAGING_SIG_RESOURCE_GROUP=${STAGING_SIG_RESOURCE_GROUP:-flatcar-image-gallery-staging}
-FLATCAR_STAGING_GALLERY_NAME=${FLATCAR_STAGING_GALLERY_NAME:-flatcar_staging}
-FLATCAR_GALLERY_NAME=${FLATCAR_GALLERY_NAME:-flatcar}
-FLATCAR_VERSION=${FLATCAR_VERSION:-3374.2.1}
-FLATCAR_CHANNEL=${FLATCAR_CHANNEL:-stable}
-FLATCAR_ARCH=${FLATCAR_ARCH:-amd64}
-FLATCAR_IMAGE_NAME=${FLATCAR_IMAGE_NAME:-flatcar-${FLATCAR_CHANNEL}-${FLATCAR_ARCH}}
-FLATCAR_IMAGE_OFFER=${FLATCAR_IMAGE_OFFER:-${FLATCAR_CHANNEL}}
-FLATCAR_IMAGE_SKU=${FLATCAR_IMAGE_SKU:-${FLATCAR_IMAGE_NAME}}
-VHD_STORAGE_SUBSCRIPTION_ID=${VHD_STORAGE_SUBSCRIPTION_ID:-${AZURE_SUBSCRIPTION_ID}}
-VHD_STORAGE_RESOURCE_GROUP_NAME=${VHD_STORAGE_RESOURCE_GROUP_NAME:-flatcar}
-VHD_STORAGE_CONTAINER_NAME=${VHD_STORAGE_CONTAINER_NAME:-publish}
-FLATCAR_COMMUNITY_GALLERY_PUBLIC_NAME_PREFIX=${FLATCAR_COMMUNITY_GALLERY_PUBLIC_NAME_PREFIX:-flatcar}
-# Regions below require explicit opt-in, so let's initially skip them from "default" regions until they are requested.
-BLACKLISTED_TARGET_REGIONS=${BLACKLISTED_TARGET_REGIONS:-polandcentral australiacentral2 brazilsoutheast centraluseuap eastus2euap eastusstg francesouth germanynorth jioindiacentral norwaywest southafricawest switzerlandwest uaecentral}
-DEFAULT_TARGET_REGIONS=$(az account list-locations -o json | jq -r '.[] | select( .metadata.regionType != "Logical" ) | .name' | sort | grep -v -E "(${BLACKLISTED_TARGET_REGIONS// /|})" | tr \\n ' ')
+PUBLISHING_SIG_RESOURCE_GROUP=${PUBLISHING_SIG_RESOURCE_GROUP:-capz-image-gallery-publishing}
+STAGING_SIG_RESOURCE_GROUP=${STAGING_SIG_RESOURCE_GROUP:-capz-image-gallery-staging}
+STAGING_GALLERY_NAME=${STAGING_GALLERY_NAME:-capz_staging}
+GALLERY_NAME=${GALLERY_NAME:-capz}
+# FLATCAR_VERSION=${FLATCAR_VERSION:-3374.2.1}
+VERSION=${VERSION:-0.0.1}
+# FLATCAR_CHANNEL=${FLATCAR_CHANNEL:-stable}
+ARCH=${ARCH:-amd64}
+# FLATCAR_IMAGE_NAME=${FLATCAR_IMAGE_NAME:-flatcar-${FLATCAR_CHANNEL}-${FLATCAR_ARCH}}
+# FLATCAR_IMAGE_OFFER=${FLATCAR_IMAGE_OFFER:-${FLATCAR_CHANNEL}}
+# FLATCAR_IMAGE_SKU=${FLATCAR_IMAGE_SKU:-${FLATCAR_IMAGE_NAME}}
+# VHD_STORAGE_SUBSCRIPTION_ID=${VHD_STORAGE_SUBSCRIPTION_ID:-${AZURE_SUBSCRIPTION_ID}}
+# VHD_STORAGE_RESOURCE_GROUP_NAME=${VHD_STORAGE_RESOURCE_GROUP_NAME:-flatcar}
+# VHD_STORAGE_CONTAINER_NAME=${VHD_STORAGE_CONTAINER_NAME:-publish}
+# FLATCAR_COMMUNITY_GALLERY_PUBLIC_NAME_PREFIX=${FLATCAR_COMMUNITY_GALLERY_PUBLIC_NAME_PREFIX:-flatcar}
+# Regions below require explicit opt-in, so exclude them from "default" regions.
+EXCLUDED_TARGET_REGIONS=${EXCLUDED_TARGET_REGIONS:-polandcentral australiacentral2 brazilsoutheast centraluseuap eastus2euap eastusstg francesouth germanynorth jioindiacentral norwaywest southafricawest switzerlandwest uaecentral}
+DEFAULT_TARGET_REGIONS=$(az account list-locations -o json | jq -r '.[] | select( .metadata.regionType != "Logical" ) | .name' | sort | grep -v -E "(${EXCLUDED_TARGET_REGIONS// /|})" | tr \\n ' ')
 TARGET_REGIONS=${TARGET_REGIONS:-${DEFAULT_TARGET_REGIONS}}
 
 # CAPI specific variables.
-KUBERNETES_SEMVER=${KUBERNETES_SEMVER:-v1.23.13}
-FLATCAR_CAPI_GALLERY_NAME=${FLATCAR_CAPI_GALLERY_NAME:-flatcar4capi}
-FLATCAR_CAPI_STAGING_GALLERY_NAME=${FLATCAR_CAPI_STAGING_GALLERY_NAME:-flatcar4capi_staging}
-FLATCAR_CAPI_IMAGE_NAME=${FLATCAR_CAPI_IMAGE_NAME:-${FLATCAR_IMAGE_NAME}-capi-${KUBERNETES_SEMVER}}
-FLATCAR_CAPI_IMAGE_OFFER=${FLATCAR_CAPI_IMAGE_OFFER:-${FLATCAR_CHANNEL}-capi}
-FLATCAR_CAPI_IMAGE_SKU=${FLATCAR_CAPI_IMAGE_SKU:-${FLATCAR_CAPI_IMAGE_NAME}}
-FLATCAR_CAPI_COMMUNITY_GALLERY_PUBLIC_NAME_PREFIX=${FLATCAR_CAPI_COMMUNITY_GALLERY_PUBLIC_NAME_PREFIX:-flatcar4capi}
+KUBERNETES_SEMVER=${KUBERNETES_SEMVER:-v1.28.3}
+# FLATCAR_CAPI_GALLERY_NAME=${FLATCAR_CAPI_GALLERY_NAME:-flatcar4capi}
+# FLATCAR_CAPI_STAGING_GALLERY_NAME=${FLATCAR_CAPI_STAGING_GALLERY_NAME:-flatcar4capi_staging}
+# FLATCAR_CAPI_IMAGE_NAME=${FLATCAR_CAPI_IMAGE_NAME:-${FLATCAR_IMAGE_NAME}-capi-${KUBERNETES_SEMVER}}
+IMAGE_NAME=capz-${KUBERNETES_SEMVER}
+# FLATCAR_CAPI_IMAGE_OFFER=${FLATCAR_CAPI_IMAGE_OFFER:-${FLATCAR_CHANNEL}-capi}
+IMAGE_OFFER=capz
+# FLATCAR_CAPI_IMAGE_SKU=${FLATCAR_CAPI_IMAGE_SKU:-${FLATCAR_CAPI_IMAGE_NAME}}
+# FLATCAR_CAPI_COMMUNITY_GALLERY_PUBLIC_NAME_PREFIX=${FLATCAR_CAPI_COMMUNITY_GALLERY_PUBLIC_NAME_PREFIX:-flatcar4capi}
 
 function publish-capz-image() {
   require-amd64-arch
@@ -62,7 +65,7 @@ function publish-capz-image() {
 
   IMAGE_NAME="${FLATCAR_CAPI_IMAGE_NAME}"
   IMAGE_VERSION="${FLATCAR_VERSION}"
-  GALLERY_NAME="${FLATCAR_CAPI_GALLERY_NAME}"
+  GALLERY_NAME="${GALLERY_NAME}"
   RESOURCE_GROUP_NAME="${PUBLISHING_SIG_RESOURCE_GROUP}"
 
   # shellcheck disable=SC2310 # This might return 1.
@@ -79,7 +82,7 @@ function publish-capz-image() {
 
   SOURCE_VERSION="/subscriptions/${AZURE_SUBSCRIPTION_ID}/resourceGroups/${STAGING_SIG_RESOURCE_GROUP}"
   SOURCE_VERSION="${SOURCE_VERSION}/providers/Microsoft.Compute/galleries"
-  SOURCE_VERSION="${SOURCE_VERSION}/${FLATCAR_CAPI_STAGING_GALLERY_NAME}/images/${IMAGE_NAME}/versions/${IMAGE_VERSION}"
+  SOURCE_VERSION="${SOURCE_VERSION}/${STAGING_GALLERY_NAME}/images/${IMAGE_NAME}/versions/${IMAGE_VERSION}"
 
   EXCLUDE_FROM_LATEST=true copy-sig-image-version
 }
@@ -87,14 +90,11 @@ function publish-capz-image() {
 function build-staging-image() {
   require-amd64-arch
 
-  # First, make sure that base Flatcar image is available.
-  ensure-flatcar-staging-sig-image-version-from-vhd
-
   login
 
-  IMAGE_NAME="${FLATCAR_CAPI_IMAGE_NAME}"
-  IMAGE_VERSION="${FLATCAR_VERSION}"
-  GALLERY_NAME="${FLATCAR_CAPI_STAGING_GALLERY_NAME}"
+  IMAGE_NAME="${IMAGE_NAME}"
+  IMAGE_VERSION="${VERSION}"
+  GALLERY_NAME="${STAGING_GALLERY_NAME}"
   RESOURCE_GROUP_NAME="${STAGING_SIG_RESOURCE_GROUP}"
 
   # shellcheck disable=SC2310 # This might return 1.
@@ -105,113 +105,53 @@ function build-staging-image() {
   ensure-resource-group
   ensure-sig
 
-  IMAGE_OFFER="${FLATCAR_CAPI_IMAGE_OFFER}"
+  IMAGE_OFFER="${IMAGE_OFFER}"
   IMAGE_PUBLISHER="${IMAGE_PUBLISHER_NAME}"
   ensure-image-definition
 
-  cat <<EOF > packer.json
-{
-  "sig_image_version": "${FLATCAR_VERSION}",
-  "kubernetes_semver": "${KUBERNETES_SEMVER}",
-  "image_name": "${IMAGE_NAME}",
-  "image_offer": "",
-  "image_publisher": "",
-  "image_sku": "",
-  "image_version": "",
-  "plan_image_offer": "",
-  "plan_image_publisher": "",
-  "plan_image_sku": "",
-  "source_sig_subscription_id": "${AZURE_SUBSCRIPTION_ID}",
-  "source_sig_resource_group_name": "${STAGING_SIG_RESOURCE_GROUP}",
-  "source_sig_name": "${FLATCAR_STAGING_GALLERY_NAME}",
-  "source_sig_image_name": "${FLATCAR_IMAGE_NAME}",
-  "source_sig_image_version": "${FLATCAR_VERSION}"
-}
-EOF
+#   cat <<EOF > images/capi/packer.json
+# {
+#   # "gallery_name": "${STAGING_GALLERY_NAME}"
+# }
+# EOF
 
+  # "sig_image_version": "${VERSION}",
+  # "image_name": "${IMAGE_NAME}",
+  # "image_offer": "${IMAGE_OFFER}",
+  # "image_publisher": "${IMAGE_PUBLISHER}",
+  # "image_sku": "${IMAGE_NAME}"
+  # "kubernetes_semver": "${KUBERNETES_SEMVER}",
+  # "image_version": "",
+  # "plan_image_offer": "",
+  # "plan_image_publisher": "",
+  # "plan_image_sku": ""
+  # "source_sig_subscription_id": "${AZURE_SUBSCRIPTION_ID}",
+  # "source_sig_resource_group_name": "${STAGING_SIG_RESOURCE_GROUP}",
+  # "source_sig_name": "${STAGING_GALLERY_NAME}",
+  # "source_sig_image_name": "${IMAGE_NAME}",
+  # "source_sig_image_version": "${VERSION}"
+
+  # Export variables expected by init-sig.sh.
   export RESOURCE_GROUP_NAME="${STAGING_SIG_RESOURCE_GROUP}"
-  export GALLERY_NAME="${FLATCAR_CAPI_STAGING_GALLERY_NAME}"
+  export GALLERY_NAME="${STAGING_GALLERY_NAME}"
   export AZURE_SUBSCRIPTION_ID
   export AZURE_LOCATION
   export AZURE_CLIENT_ID
   export AZURE_CLIENT_SECRET
-  export PACKER_VAR_FILES=packer.json
+  # export PACKER_VAR_FILES=packer.json
 
   # I'd recommend running in debug mode when running interactively, as Packer tends to produce hard to debug
   # error messages.
-  export DEBUG=true
-  export PACKER_LOG=1
+  # export DEBUG=true
+  # export PACKER_LOG=1
 
-  make -C ../.. build-azure-sig-ubuntu-2204
-}
-
-function publish-flatcar-image() {
-  ensure-flatcar-staging-sig-image-version-from-vhd
-
-  login
-
-  IMAGE_NAME="${FLATCAR_IMAGE_NAME}"
-  IMAGE_VERSION="${FLATCAR_VERSION}"
-  GALLERY_NAME="${FLATCAR_GALLERY_NAME}"
-  RESOURCE_GROUP_NAME="${PUBLISHING_SIG_RESOURCE_GROUP}"
-
-  # shellcheck disable=SC2310 # This might return 1.
-  if sig-image-version-exists; then
-    return
-  fi
-
-  ensure-resource-group
-
-  PUBLIC_NAME_PREFIX="${FLATCAR_COMMUNITY_GALLERY_PUBLIC_NAME_PREFIX}" ensure-community-sig
-
-  IMAGE_OFFER="${FLATCAR_IMAGE_OFFER}" \
-    IMAGE_PUBLISHER="${IMAGE_PUBLISHER_NAME}" \
-    ensure-image-definition
-
-  SOURCE_VERSION="/subscriptions/${AZURE_SUBSCRIPTION_ID}/resourceGroups/${STAGING_SIG_RESOURCE_GROUP}"
-  SOURCE_VERSION="${SOURCE_VERSION}/providers/Microsoft.Compute/galleries"
-  SOURCE_VERSION="${SOURCE_VERSION}/${FLATCAR_STAGING_GALLERY_NAME}/images/${IMAGE_NAME}/versions/${IMAGE_VERSION}"
-
-  copy-sig-image-version
-}
-
-function ensure-flatcar-staging-sig-image-version-from-vhd() {
-  login
-
-  IMAGE_NAME="${FLATCAR_IMAGE_NAME}"
-  IMAGE_VERSION="${FLATCAR_VERSION}"
-  GALLERY_NAME="${FLATCAR_STAGING_GALLERY_NAME}"
-  RESOURCE_GROUP_NAME="${STAGING_SIG_RESOURCE_GROUP}"
-
-  # shellcheck disable=SC2310 # This might return 1.
-  if sig-image-version-exists; then
-    return
-  fi
-
-  ensure-resource-group
-  ensure-sig
-
-  IMAGE_OFFER="${FLATCAR_IMAGE_OFFER}" IMAGE_PUBLISHER="${IMAGE_PUBLISHER_NAME}" ensure-image-definition
-
-  STORAGE_ACCOUNT_ID="/subscriptions/${AZURE_SUBSCRIPTION_ID}/resourceGroups/${VHD_STORAGE_RESOURCE_GROUP_NAME}"
-  STORAGE_ACCOUNT_ID="${STORAGE_ACCOUNT_ID}/providers/Microsoft.Storage/storageAccounts/${VHD_STORAGE_ACCOUNT_NAME}"
-
-  VHD_URI="https://${VHD_STORAGE_ACCOUNT_NAME}.blob.core.windows.net/${VHD_STORAGE_CONTAINER_NAME}"
-  VHD_URI="${VHD_URI}/flatcar-linux-${IMAGE_VERSION}-${FLATCAR_CHANNEL}-${FLATCAR_ARCH}.vhd"
-
-  az sig image-version create \
-    --resource-group "${RESOURCE_GROUP_NAME}" \
-    --gallery-name "${GALLERY_NAME}" \
-    --gallery-image-definition "${IMAGE_NAME}" \
-    --gallery-image-version "${IMAGE_VERSION}" \
-    --os-vhd-storage-account "${STORAGE_ACCOUNT_ID}" \
-    --os-vhd-uri "${VHD_URI}"
+  make -C images/capi build-azure-sig-ubuntu-2204
 }
 
 # Below are utility functions.
 function require-amd64-arch() {
-  if [[ "${FLATCAR_ARCH}" != "amd64" ]]; then
-    echo "Unsupported architecture '${FLATCAR_ARCH}'. Only supported is 'amd64'."
+  if [[ "${ARCH}" != "amd64" ]]; then
+    echo "Unsupported architecture '${ARCH}'. Only 'amd64' is supported."
     exit 1
   fi
 }
@@ -267,7 +207,7 @@ function ensure-image-definition() {
 
   local architecture
 
-  case "${FLATCAR_ARCH}" in
+  case "${ARCH}" in
     amd64)
       architecture=x64
       ;;
@@ -275,7 +215,7 @@ function ensure-image-definition() {
       architecture=Arm64
       ;;
     *)
-      echo "Unsupported architecture: '${FLATCAR_ARCH}'"
+      echo "Unsupported architecture: '${ARCH}'"
       exit 1
       ;;
   esac
@@ -335,10 +275,8 @@ if [[ $# -eq 0 ]]; then
 usage: $0 <action>
 
 Available actions:
-  - ensure-flatcar-staging-sig-image-version-from-vhd - Creates Flatcar image in staging SIG from VHD image.
-  - publish-flatcar-image - Publishes Flatcar image to community SIG from staging SIG.
   - build-staging-image - Builds CAPZ image using image-builder to staging SIG.
-  - publish-capz-image - Publishes Flatcar CAPI image to community SIG from staging SIG.
+  - publish-capz-image - Publishes CAPZ image to community SIG from staging SIG.
 EOF
 
   exit 0
